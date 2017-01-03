49ac953087c5bcb2e75de6686ffd6d5b7bec2a04
Fabien Traventhal <fabien.traventhal@gmail.com>
2017-01-03 23:55:35 +0000

    project finished

--- server.js
+++ server.js
@@ -1,19 +1,67 @@
 var express = require('express')
-var strftime = require('strftime')
-var ip = require("ip")
-var platform = require("platform")
+var url = 'mongodb://localhost:27017/shorturl'
+var mongo = require('mongodb').MongoClient
+var redirect = require('express-simple-redirect');
 var app = express()
-app.get('/whoami', function (req, res) {
-    // var lang = require("get-browser-language")()
-    console.log(req.acceptsLanguages)
-    var to_return = {
-            ip: ip.address(),
-            language: req.acceptsLanguages()[0],
-            os: platform.os
-        }
-    
-    res.send(JSON.stringify(to_return))
+
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+
+
+
+app.get('/:path', function (req, res) {
+    var path_given = req.params.path
+    console.log(path_given)
+mongo.connect(url, function(err, db) {
+    if (err) throw err
+    var col = db.collection('urls')
+  col.find({
+      _id: path_given
+    }).toArray(function(err, documents) {
+    if(err) return console.log(err)
+    if (documents){
+        console.log(documents[0].original_url)
+        app.use(redirect({
+          '/my-url': documents[0].original_url  // Internal redirect to /about page 
+        }));
+        // res.writeHead(301,
+        //   {Location: documents[0].original_url});
+        res.end();
+    }
+    db.close()
+    })
 })
+    
+})
+
+app.get('/shorten/:url*', function (req, res) {
+    var url_given = req.params.url+req.params['0']
+mongo.connect(url, function(err, db) {
+    if (err) throw err
+    var col = db.collection('urls')
+    var link_id = S4()
+  var doc = {
+      original_url: url_given,
+      _id: link_id
+    }
+  col.insert(doc, function(err, data) {
+    if (err) throw err
+    
+    var to_return = {
+            url_given: url_given,
+            url_shorten: "https://fcc-api-projects-as97one.c9users.io/"+link_id
+        }
+    console.log(data)
+    res.send(JSON.stringify(to_return))
+    db.close()
+  })
+})
+    
+})
+
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
